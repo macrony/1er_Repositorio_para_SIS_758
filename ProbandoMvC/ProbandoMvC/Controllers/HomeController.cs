72c44d2212c05553d2043c2c735fc212863aaf38
Macrony <macrony_rp97.tk@hotmail.com>
2013-05-20 10:36:13 -0400

    Tercera Prueba

--- ProbandoMvC/ProbandoMvC/Controllers/HomeController.cs
+++ ProbandoMvC/ProbandoMvC/Controllers/HomeController.cs
@@ -8,6 +8,11 @@ namespace ProbandoMvC.Controllers
 {
     public class HomeController : Controller
     {
+        public ActionResult Regitro()
+        {
+            return View();
+        }
+        
         public ActionResult Index()
         {
             ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
